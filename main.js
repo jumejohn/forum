51c46a1f6d5ad271e71971d63639f64f5291c156
Justin Johnson <jumejohn@live.com>
2022-08-02 15:03:52 -0700

    clean up some spacing and naming

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
+// create new posts
 document.getElementById('submit').addEventListener('click', function () {
   var userInputName = document.getElementById('name').value;
   var userInputMessage = document.getElementById('message').value;
-
-
+  
   var newPostDiv = document.createElement('div');
 
   var newMessageP = document.createElement('p');
@@ -10,27 +10,27 @@ document.getElementById('submit').addEventListener('click', function () {
   newMessageP.appendChild(newMessageText);
 
   var newNameP = document.createElement('p');
-  var newNameText = document.createTextNode(`Posted By ` + userInputName);
+  var newNameText = document.createTextNode(`Posted By ` + userInputName);  
   newNameP.appendChild(newNameText);
-
-  var newUpBtn = document.createElement('button');
-  newUpBtn.setAttribute('class', "btn btn-primary");
-  var upBtnText = document.createTextNode('Upvote');
-  newUpBtn.appendChild(upBtnText);
   
-  var newDownBtn = document.createElement('button');
-  newDownBtn.setAttribute('class', "btn btn-primary");
+  
+  var upBtn = document.createElement('button');
+  upBtn.setAttribute('class', "btn btn-success");
+  var upBtnText = document.createTextNode('Upvote');
+  upBtn.appendChild(upBtnText);
+  
+  var downBtn = document.createElement('button');
+  downBtn.setAttribute('class', "btn btn-danger");
   var downBtnText = document.createTextNode('Downvote');
-  newDownBtn.appendChild(downBtnText);
+  downBtn.appendChild(downBtnText);
   
   var newHr = document.createElement('hr');
 
   newPostDiv.append(newMessageP);
   newPostDiv.append(newNameP);
-  newPostDiv.append(newUpBtn);
-  newPostDiv.append(newDownBtn);
+  newPostDiv.append(upBtn);
+  newPostDiv.append(downBtn);
   newPostDiv.append(newHr);
 
   document.getElementsByClassName('posts')[0].append(newPostDiv);
 });
-
